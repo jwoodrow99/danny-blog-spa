5119a943079c947ec24083d12d7baf2d1f997d67
Jack Woodrow <jackwoodrow99@gmail.com>
2023-04-16 18:55:04 -0400

    Basic login system and request

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,17 @@
 import type { Component } from 'solid-js';
+import { createSignal, createEffect, useContext } from 'solid-js';
+import { GlobalContext } from '../GlobalContext';
 
 const HomePage: Component = () => {
+	const { authenticated, setAuthenticated, user, setUser }: any =
+		useContext(GlobalContext);
 	return (
 		<>
 			<div>Home Page</div>
+			<div>
+				{authenticated() && <p>Logged In</p>}
+				{!authenticated() && <p>NOT Logged In</p>}
+			</div>
 		</>
 	);
 };
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -2,13 +2,60 @@ import type { Component } from 'solid-js';
 import { createSignal, createEffect, useContext } from 'solid-js';
 import { GlobalContext } from '../GlobalContext';
 
+import http from '../http';
+
 const LoginPage: Component = () => {
 	const { authenticated, setAuthenticated, user, setUser }: any =
 		useContext(GlobalContext);
 
+	const [email, setEmail] = createSignal('');
+	const [password, setPassword] = createSignal('');
+
+	const login = () => {
+		http
+			.post('/auth/login/', {
+				email: email(),
+				password: password(),
+			})
+			.then((response) => {
+				localStorage.setItem('access_token', response.data.access_token);
+				setAuthenticated(true);
+				// localStorage.setItem('user', JSON.stringify(response.data.user));
+				// setUser(response.data.user);
+				// navigate('/');
+			})
+			.catch((error) => {
+				console.log(error.response.status);
+				console.log(error.response.data);
+			});
+
+		setEmail('');
+		setPassword('');
+	};
+
 	return (
 		<>
-			<div>Login Page</div>
+			<div class="flex flex-col space-y-3 w-1/2 mx-auto">
+				<input
+					class="text-black"
+					type="text"
+					value={email()}
+					onChange={(e) => setEmail(e.currentTarget.value)}
+				/>
+				<input
+					class="text-black"
+					type="text"
+					value={password()}
+					onChange={(e) => setPassword(e.currentTarget.value)}
+				/>
+				<button
+					onClick={() => {
+						login();
+					}}
+				>
+					Login
+				</button>
+			</div>
 		</>
 	);
 };
