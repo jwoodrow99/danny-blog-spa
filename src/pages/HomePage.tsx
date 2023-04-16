e7b73eec21a0ca937c5a55276647ac46f74de036
Jack Woodrow <jackwoodrow99@gmail.com>
2023-04-16 00:01:05 -0400

    search on homepage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -8,9 +8,11 @@ import BlogPostListItemComponent from '../components/BlogPostListItemComponent';
 
 const HomePage: Component = () => {
 	const [blogs, setBlogs] = createSignal([]);
+	const [search, setSearch] = createSignal('');
 
 	createEffect(() => {
 		console.log('Render: HomePage');
+
 		http
 			.get('/blog')
 			.then((response) => {
@@ -22,8 +24,40 @@ const HomePage: Component = () => {
 			});
 	});
 
+	const searchBlogs = () => {
+		http
+			.get('/blog', { params: { search: `title:${search()}` } })
+			.then((response) => {
+				setBlogs(response.data.blogs);
+			})
+			.catch((error) => {
+				console.log(error.response.status);
+				console.log(error.response.data);
+			});
+		setSearch('');
+	};
+
 	return (
 		<>
+			<div class="flex flex-row space-x-5 my-10 px-5">
+				<input
+					class="text-black w-full rounded-full px-3 py-1"
+					type="text"
+					id="email"
+					value={search()}
+					onChange={(e) => setSearch(e.currentTarget.value)}
+				/>
+				<button
+					class="btn-primary px-5"
+					type="button"
+					onclick={() => {
+						searchBlogs();
+					}}
+				>
+					search
+				</button>
+			</div>
+
 			<div class="flex flex-col space-y-5">
 				{blogs().map((blog: any) => {
 					return (
