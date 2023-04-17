206599116dfa13a5f865afbca9c0ff83f5ed772f
Jack Woodrow <jackwoodrow99@gmail.com>
2023-04-17 13:28:27 -0400

    list blogs on homepage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,16 +2,33 @@ import type { Component } from 'solid-js';
 import { createSignal, createEffect, useContext } from 'solid-js';
 import { GlobalContext } from '../GlobalContext';
 
+import http from '../http';
+
 const HomePage: Component = () => {
 	const { authenticated, setAuthenticated, user, setUser }: any =
 		useContext(GlobalContext);
+
+	const [blogs, setBlogs] = createSignal([]);
+
+	createEffect(() => {
+		http
+			.get('/blog', { params: { order_by: 'created_at:desc' } })
+			.then((response) => {
+				setBlogs(response.data.blogs);
+			})
+			.catch((error) => {
+				console.log(error.response.status);
+				console.log(error.response.data);
+			});
+	});
+
 	return (
 		<>
-			<div>Home Page</div>
-			<div>
-				{authenticated() && <p>Logged In</p>}
-				{!authenticated() && <p>NOT Logged In</p>}
-			</div>
+			<ul>
+				{blogs().map((blog: any) => {
+					return <li>{blog.title}</li>;
+				})}
+			</ul>
 		</>
 	);
 };
